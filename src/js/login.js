0e6bdc2792f5b8320c040951a44990e9ab43abc1
Raissa <raissa.vilemon@gmail.com>
2023-01-25 21:14:47 -0300

    Adição da página login.js Albino

--- src/js/login.js
+++ src/js/login.js
@@ -1,142 +1,93 @@
-import {addUser} from "./users.js";
-import {getUsers} from "./ls.js";
+import { loginTemp, senhaTemp, loginAdmin } from "./modules/users.js";
+import {
+  validateAccess,
+  validateEmail,
+  validateCase,
+} from "./modules/validation.js";
 
-export function addRegisterAction() {
-    let btnRegitration = document.getElementById("btn__registeration");
+const btn_login = document.querySelector("#btn_login");
+const btn_register = document.querySelector("#btn_register");
 
-    if (!btnRegitration) {
-        return;
-    }
+/* Operações do Login e autenticação. */
+btn_login.addEventListener("click", () => {
+  let email = document.querySelector("#username").value;
+  let username = document.querySelector("#username").value;
+  let password = document.querySelector("#password").value;
 
-    btnRegitration.addEventListener("click", event => {
-        event.preventDefault();
-        let registrationForm = new FormData(document.getElementById("form-registration"));
+  username.includes("@") && username.includes(".com")
+    ? localStorage.setItem("mail", email)
+    : localStorage.setItem("usuario", username);
+  localStorage.setItem("senha", password);
 
-        let name = registrationForm.get("name");
-        let email = registrationForm.get("email");
-        let password = registrationForm.get("password");
+  const usuario = localStorage.getItem("usuario");
+  const senha = localStorage.getItem("senha");
 
-        if(validationRegistration(name, email, password)){
-            addUser(name, email, password);
+  if (usuario.length < 3) {
+    alert("Por favor, preencha adequadamente os campos.");
+    localStorage.clear();
+  } else if (validateAccess(usuario, loginAdmin) && senha == senhaTemp) {
+    alert("Login de adimnistrador bem sucedido, você será redirecionado.");
+    location.href = "../pages/admin/home_admin.html";
+  } else if (validateAccess(usuario, loginTemp) && senha == senhaTemp) {
+    alert("Login bem sucedido, você será redirecionado.");
+    location.href = "../../index.html";
+  } else {
+    alert("Por favor, verifique suas credenciais.");
+    localStorage.clear();
+  }
+});
 
-            setAlert("Usuário Cadastrado", "register-alert", "success");
-        }
-    })
-}
+/* Operações do registro */
+btn_register.addEventListener("click", () => {
+  let email = document
+    .querySelector("#new_userEmail")
+    .value.trim()
+    .replace(/'/g, "")
+    .replace(/"/g, "")
+    .replace(" ", "");
+  let username = document
+    .querySelector("#new_userUsuario")
+    .value.trim()
+    .replace(/'/g, "")
+    .replace(/"/g, "")
+    .replace(" ", "");
+  let password = document
+    .querySelector("#new_userPassword")
+    .value.trim()
+    .replace(/'/g, "")
+    .replace(/"/g, "")
+    .replace(" ", "");
 
-export function addLoginAction() {
-    let btnLogin = document.getElementById("btn__login");
+  localStorage.setItem("mail", email);
+  localStorage.setItem("usuario", username);
+  localStorage.setItem("senha", password);
 
-    if (!btnLogin) {
-        return;
-    }
+  const mail = localStorage.getItem("mail");
+  const usuario = localStorage.getItem("usuario");
+  const senha = localStorage.getItem("senha");
 
-    btnLogin.addEventListener("click", event => {
-        event.preventDefault();
-
-        let loginForm = new FormData(document.getElementById("form-login"));
-        setLoggedUser(loginForm.get("email"), loginForm.get("password"))
-    })
-}
-
-function validationLogin(email, password) {
-    if (email === "") {
-        setAlert("Campo E-mail não pode ser vazio", "login-alert", "error");
-        return false;
-    }
-
-    if (email.match(/^([\w]+[\w$-/:-?{-~!"^_`\[\]]*)@([\w]+)\.(\w+)$/) === null) {
-        setAlert("E-mail inválido", "login-alert", "error");
-        return false;
-    }
-
-    if (password === "") {
-        setAlert("Campo Senha não pode ser vazio", "login-alert", "error");
-        return false;
-    }
-
-    let user = getUsers()[email];
-
-    if (!user) {
-        setAlert("Usuário não encontrado", "login-alert", "error");
-        return false;
-    }
-
-    if (user.password !== password) {
-        setAlert("Senha incorreta", "login-alert", "error");
-        return false;
-    }
-
-
-    return user;
-}
-
-function validationRegistration(name, email, password) {
-    if (email === "") {
-        setAlert("Campo E-mail não pode ser vazio", "register-alert", "error");
-        return false;
-    }
-
-    if (email.match(/^([\w]+[\w$-/:-?{-~!"^_`\[\]]*)@([\w]+)\.(\w+)$/) === null) {
-        setAlert("E-mail inválido", "register-alert", "error");
-        return false;
-    }
-
-    if (getUsers()[email]) {
-        setAlert("E-mail já cadastrado", "register-alert", "error");
-        return false;
-    }
-
-    if (name === "") {
-        setAlert("Campo Nome não pode ser vazio", "register-alert", "error");
-        return false;
-    }
-
-    if (password === "") {
-        setAlert("Campo Senha não pode ser vazio", "register-alert", "error");
-        return false;
-    }
-
-
-    return true;
-}
-
-export function setLoggedUser(email, password) {
-    let user = validationLogin(email, password)
-    if (user) {
-        localStorage.setItem("loggedUser", JSON.stringify(user));
-
-
-        if (user.roles === "ROLE_ADMIN") {
-            window.location.href = "https://texpertsg3.github.io/Hotel-DEVaneio/src/pages/admin/home_admin.html";
-            return;
-        }
-
-        window.location.href = "https://texpertsg3.github.io/Hotel-DEVaneio/";
-
-    }
-}
-
-export function setAlert(message, element, code) {
-    let alert = document.getElementById(element);
-
-    if (!alert) {
-        return;
-    }
-
-    if (code === "error") {
-        alert.parentElement.classList.add("bg-red-lighten-6");
-        alert.parentElement.classList.add("color-red-darken-6");
-        alert.parentElement.classList.remove("bg-green-lighten-6");
-        alert.parentElement.classList.remove("color-green-darken-6");
-    }
-    else if (code === "success") {
-        alert.parentElement.classList.remove("bg-red-lighten-6");
-        alert.parentElement.classList.remove("color-red-darken-6");
-        alert.parentElement.classList.add("bg-green-lighten-6");
-        alert.parentElement.classList.add("color-green-darken-6");
-    }
-
-    alert.innerHTML = message;
-    alert.parentElement.classList.remove("d-none");
-}
+  if (
+    usuario.length < 3 ||
+    validateEmail(mail) == false ||
+    validateCase(mail)
+  ) {
+    alert(
+      "Por favor, preencha adequadamente os campos solicitados.(Sem aspas e letras maiúsculas.)"
+    );
+    localStorage.clear();
+  } else if (
+    validateAccess(usuario, loginTemp) ||
+    validateAccess(mail, loginTemp)
+  ) {
+    alert(
+      "Desculpe, esse usuário já existe. Por favor, escolha outro, ou escolha logar ao lado."
+    );
+    localStorage.clear();
+  } else {
+    loginTemp.push(mail);
+    loginTemp.push(usuario);
+    loginTemp.push(senha);
+    alert("Resgistro concluído com sucesso.");
+    location.href = "../../index.html";
+  }
+});
