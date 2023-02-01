1e71a80da9da69f06236f75621a8c5e6032b87ad
TexpertsG3 <texpertsg3@gmail.com>
2023-01-31 22:31:36 -0300

    Implementar o Vue Routes.

---
+++ src/views/AboutApp.vue
@@ -0,0 +1,17 @@
+<script>
+
+import About from "./views/about.vue";
+
+export default {
+    name: "AboutApp",
+    components: {
+        About
+    },
+};
+</script>
+
+<template>
+<HeaderContent />
+  <router-view />
+  <FooterContent />
+</template>
