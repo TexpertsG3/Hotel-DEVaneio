247164f8e2eed154ae1088ee916fa8b85ac68575
albino-marques <albinomarques01@gmail.com>
2023-02-01 17:50:34 -0300

    Adição de algumas rotas e tentativa se reajuste do site.

--- src/components/RoomsCarousel.vue
+++ src/components/RoomsCarousel.vue
@@ -1,10 +1,12 @@
 <!-- eslint-disable prettier/prettier -->
  <template>
-
+    <header class="row j-center col-12-xl">
+      <h2 class="font-size-extra-large">Nossas Acomodações</h2>
+    </header>
     <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
       <SplideSlide>
         <img src="@/assets/images/accommodations/quarto-c.jpg" alt="Sample 1" />
-        <div class="row col-12-xl gap-2 col-2-lg">
+        <div class="row col-12-xl gap-2 col-2-lg ">
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus nemo alias a impedit praesentium
               aliquam, ratione nisi expedita libero deserunt dolore maiores vel eveniet soluta provident porro
