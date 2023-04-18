8081b3e231179a670951c802051c08deebc386ef
Raissa-Reis-Lopes <raissa.vilemon@gmail.com>
2023-04-18 01:06:26 -0300

    WIP responsividade Home, seção About

--- src/views/HomeContent.vue
+++ src/views/HomeContent.vue
@@ -1,11 +1,15 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
   <main>
-    <section class="container pt-3 pb-3" id="about">
+    <!-- About Home tablet/desktop -->
+    <section class="aboutHomeTablet container pt-3 pb-3" id="about">
       <div class="row j-space-between">
-        <div class="row col-4-xl a-center gap-1">
+        <!--  -->
+        <div class="infoAboutHome row col-4-xl a-center gap-1">
           <header class="col-12-xl">
-            <h2 class="font-size-extra-large">Sobre o nosso Hotel</h2>
+            <h2 class="fontHeader font-size-extra-large">
+              Sobre o nosso Hotel
+            </h2>
           </header>
           <article class="row gap-1">
             <p class="">
@@ -24,7 +28,7 @@
             </div>
           </article>
         </div>
-        <div class="row col-6-xl">
+        <div class="imgAboutHome row col-6-xl">
           <img
             src="@/assets/images/home/hotel-small.jpg"
             alt="Fachada do Hotel"
@@ -33,8 +37,34 @@
         </div>
       </div>
     </section>
+    <!-- About home mobile -->
+    <section class="aboutHomeMobile container pt-3 pb-3" id="aboutMobile">
+      <div class="row">
+        <!--  -->
+        <div class="row col-12-xs a-center gap-1">
+          <header class="col-12-xs">
+            <h2 class="fontHeader font-size-regular">Sobre o nosso Hotel</h2>
+          </header>
+          <article class="row gap-1 col-12-xs j-center">
+            <p class="">
+              Venha se divertir e descansar com toda a sua família no Hotel
+              DEVaneio. Experiências incríveis te aguardam.
+            </p>
+            <img
+              src="@/assets/images/home/hotel-small.jpg"
+              alt="Fachada do Hotel"
+              class="img col-8-xs"
+            />
+          </article>
+        </div>
+        <div class="row col-12-xs mt-1 j-center">
+          <a class="btn-primary color-white" href="/about">Saiba mais</a>
+        </div>
+      </div>
+    </section>
 
-    <div class="container row gap-3 pt-3 pb-3">
+    <!-- Acomodações Home tablet/desktop -->
+    <div class="roomsHomeTablet container row gap-3 pt-3 pb-3">
       <section class="row">
         <article class="row a-center a-item-center j-space-between">
           <div class="col-6-xl">
@@ -53,6 +83,31 @@
                 Temporibus nemo alias a impedit praesentium aliquam, ratione
                 nisi expedita libero deserunt dolore maiores vel eveniet soluta
                 provident porro accusamus ducimus voluptatum?
+              </p>
+              <a href="/rooms" class="btn-primary color-white">Saiba Mais</a>
+            </div>
+          </div>
+        </article>
+      </section>
+    </div>
+    <!-- Acomodações Home mobile-->
+    <div class="roomsHomeMobile container row gap-3 pt-3 pb-3">
+      <section class="row">
+        <article class="row a-center a-item-center j-space-between">
+          <div class="col-6-xl">
+            <img
+              src="../assets/images/accommodations/quarto-c2.jpg"
+              alt=""
+              class="img col-12-xl"
+            />
+          </div>
+          <div class="row col-4-xl gap-2 a-center a-item-center">
+            <h2 class="font-size-extra-large">Nossas Acomodações</h2>
+
+            <div class="row col-12-xl gap-2">
+              <p>
+                Nossas acomodações foram planejadas para oferecer o máximo
+                conforto e tecnologia para os nossos hóspedes.
               </p>
               <a href="/rooms" class="btn-primary color-white">Saiba Mais</a>
             </div>
